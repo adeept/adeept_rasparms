d70238f73b1ffd4ae2b20f5e54a9210e11a9d847
Devin <support@adeept.com>
2022-03-23 10:59:06 +0800

    update

--- CourseCode/03Servo180/180Servo_01.py
+++ CourseCode/03Servo180/180Servo_01.py
@@ -4,9 +4,9 @@ import time
 pwm = Adafruit_PCA9685.PCA9685()	# Instantiate the object used to control the PWM
 pwm.set_pwm_freq(50)	# Set the frequency of the PWM signal
 
-# Make the servo connected to the No. 3 servo port on the RobotHAT drive board reciprocate
+# Make the servo connected to the No.0 servo port on the RobotHAT drive board reciprocate
 while 1:
-	pwm.set_pwm(3, 0, 300)
+	pwm.set_pwm(0, 0, 300)
 	time.sleep(1)
-	pwm.set_pwm(3, 0, 400)
+	pwm.set_pwm(0, 0, 400)
 	time.sleep(1)
--- CourseCode/03Servo180/180Servo_02.py
+++ CourseCode/03Servo180/180Servo_02.py
@@ -6,8 +6,8 @@ pwm.set_pwm_freq(50)    # Set the frequency of the PWM signal
   
 while 1:
       for i in range(0,100):    # Make the servo move slowly from 300 to 400
-          pwm.set_pwm(3, 0, (300+i))
+          pwm.set_pwm(0, 0, (300+i))
           time.sleep(0.05)
       for i in range(0,100):    # Make the servo move slowly from 400 to 300
-          pwm.set_pwm(3, 0, (400-i))
+          pwm.set_pwm(0, 0, (400-i))
           time.sleep(0.05)
